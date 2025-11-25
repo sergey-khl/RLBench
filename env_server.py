16ac44ffc29020db6749c24b767e5bb3ba458390
Serg <serg@gmail.com>
2025-11-25 13:50:51 -0700

    actually dont want headless

--- env_server.py
+++ env_server.py
@@ -49,7 +49,7 @@ def run_server():
 
     action_mode = EndEffectorActionMode()
     env = Environment(
-        action_mode, '', obs_config, headless=True)
+        action_mode, '', obs_config, headless=False)
     env.launch()
 
     task = env.get_task(CloseMicrowave)
